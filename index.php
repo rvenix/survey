f9d32e1bb635cb6e4f81540508b2a5a658c31db3
Maciej Roguski <mroguski@wp.pl>
2013-02-21 08:25:22 +0100

    nowyObiekt

--- index.php
+++ index.php
@@ -1,19 +1,32 @@
 <?php
+
+class nowyObiekt{
+    private $tablica = array();
+    
+    public function __construct() {
+        $this->tablica[] = 'nowy element1';
+    }
+}
+
 class test{
     private $zmienna1;
     private $zmienna2;
     private $nowazmienna;
+    private $nowy;
     
     function __construct() {
         $this->zmienna1 = "zmienna1";
         $this->zmienna2 = "zmienna2";
         $this->nowazmienna = "nowa zmienna";
+        $this->nowy = new nowyObiekt();
     }
     
     function __toString() {
         return $this->zmienna1.' '.$this->zmienna2.'<br>';
     }
 }
+
+
 
 $obiekt = new test();
 
