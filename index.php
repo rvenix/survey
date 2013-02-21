b009ca6965c5181afcf19a2c8b840496e83e4945
Maciej Roguski <mroguski@wp.pl>
2013-02-21 08:04:35 +0100

    nowa zmienna

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 class test{
     private $zmienna1;
     private $zmienna2;
+    private $nowazmienna;
     
     function __construct() {
         $this->zmienna1 = "zmienna1";
         $this->zmienna2 = "zmienna2";
+        $this->nowazmienna = "nowa zmienna";
     }
     
     function __toString() {
